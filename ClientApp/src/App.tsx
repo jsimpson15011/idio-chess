210fff857b1916541ea8b23894c347eedcbb3a23
jsimpson15011 <joseph.franklin.simpson@gmail.com>
2023-06-16 15:29:48 -0600

    Remove unnecessary components

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
 import ChessBoard from "./components/ChessBoard/ChessBoard";
 
 import './custom.css'
